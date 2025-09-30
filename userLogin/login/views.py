fe39f47753bb8f3936c077bd645c4f05165c6397
valbmg <21100904@usc.edu.ph>
2025-09-30 21:38:04 +0800

    feature(login): complete app setup

--- userLogin/login/views.py
+++ userLogin/login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Welcome to Momentum!")
+
+def login_page(request):
+    return render(request, 'login/login.html')
+
+def dashboard(request):
+    return render(request, 'login/dashboard.html')
